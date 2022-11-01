fd4e91d6010db1ffc8fc65ac9ae9e9ae3c05eeba
joey <hammar@micropayment.de>
2022-11-01 15:02:02 +0100

    logo modified - input functions

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -4,34 +4,31 @@
 <template>
    <div class="main">
         <div class="inside">
-            <h1>Home</h1>
+            <div>
+                <b-form>
+                    <b-form-group id="input-group-1" label="Email address:" label-for="input-1" description="We'll never share your email with anyone else.">
+                        <b-form-input id="input-1" type="email" placeholder="Enter email" required ></b-form-input>
+                    </b-form-group>
 
+                    <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+                        <b-form-input id="input-2"  placeholder="Enter name" required></b-form-input>
+                    </b-form-group>
 
-        <div>
-            <b-form>
-                <b-form-group id="input-group-1" label="Email address:" label-for="input-1" description="We'll never share your email with anyone else.">
-                    <b-form-input id="input-1" type="email" placeholder="Enter email" required ></b-form-input>
-                </b-form-group>
+                    <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+                        <b-form-select id="input-3"   required></b-form-select>
+                    </b-form-group>
 
-                <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-                    <b-form-input id="input-2"  placeholder="Enter name" required></b-form-input>
-                </b-form-group>
+                    <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+                        <b-form-checkbox-group id="checkboxes-4" :aria-describedby="ariaDescribedby">
+                        <b-form-checkbox value="me">Check me out</b-form-checkbox>
+                        <b-form-checkbox value="that">Check that out</b-form-checkbox>
+                        </b-form-checkbox-group>
+                    </b-form-group>
 
-                <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-                    <b-form-select id="input-3"   required></b-form-select>
-                </b-form-group>
-
-            <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-                <b-form-checkbox-group id="checkboxes-4" :aria-describedby="ariaDescribedby">
-                <b-form-checkbox value="me">Check me out</b-form-checkbox>
-                <b-form-checkbox value="that">Check that out</b-form-checkbox>
-                </b-form-checkbox-group>
-            </b-form-group>
-
-            <b-button type="submit" variant="primary">Submit</b-button>
-            <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-        </div>
+                    <b-button type="submit" variant="primary">Submit</b-button>
+                    <b-button type="reset" variant="danger">Reset</b-button>
+                </b-form>
+            </div>
         </div>
    </div>
 </template>
@@ -40,6 +37,10 @@
 
     .main {
         background-color: $background-color;
+        min-height: 70vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
         .inside {
             @include width-margin;
         }
